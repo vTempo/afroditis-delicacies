c0665872d0f93235f1b555bfa6aa12f4eb47cc57
Vincent <vincentkao2003@gmail.com>
2023-06-22 10:27:30 -0700

    initial file skeletons

---
+++ app.js
@@ -0,0 +1,43 @@
+/**
+ * header comment
+ */
+"use strict";
+
+const express = require("express");
+const app = express();
+
+const multer = require("multer");
+
+const sqlite3 = require("sqlite3");
+const sqlite = require("sqlite");
+
+const cookieParser = require("cookie-parser");
+
+// for application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true})); // built-in middleware
+// for application/json
+app.use(express.json()); // built-in middleware
+// for multipart/form-data (required with FormData)
+app.use(multer().none()); // requires the "multer" module
+
+// for cookies
+app.use(cookieParser());
+
+
+
+/**
+ * Establishes a database connection to the database and returns the database object.
+ * Any errors that occur should be caught in the function that calls this one.
+ * @returns {sqlite3.Database} - The database object for the connection.
+ */
+async function getDBConnection() {
+  const db = await sqlite.open({
+    filename: "vehicles.db",
+    driver: sqlite3.Database
+  });
+  return db;
+}
+
+app.use(express.static("public"));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
