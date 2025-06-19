8559e3b2666eb02632e0263a0a54653e06f47b6e
Ilias Kladakis <iliask@uw.edu>
2025-06-18 22:21:17 -0700

    there is color

--- src/components/utils/header.tsx
+++ src/components/utils/header.tsx
@@ -5,15 +5,14 @@ const Header: React.FC = () => {
     return (
         <header className="container-fluid">
             <nav className="navbar">
-                <div className="nav-account">
-                    <div className="nav-item">
-                        <a href="/login" className="nav-link">Login</a>
-                    </div>
-                    <div className="nav-item">
-                        <a href="/register" className="nav-link">Register</a>
+                {/* Left side - Logo */}
+                <div className="navbar-brand">
+                    <div className="logo-circle">
+                        <div className="logo-text">AD</div>
                     </div>
                 </div>
 
+                {/* Center - Title and Navigation */}
                 <div id="nav-middle">
                     <h1>Afroditi's Delicacies</h1>
                     <div id="navbar">
@@ -25,19 +24,27 @@ const Header: React.FC = () => {
                                 <a href="/menu" className="nav-link">Menu</a>
                             </div>
                             <div className="nav-item">
-                                <a href="/about" className="nav-link">About</a>
+                                <a href="/about" className="nav-link">About Us</a>
                             </div>
                             <div className="nav-item">
-                                <a href="/contact" className="nav-link">Contact</a>
+                                <a href="/contact" className="nav-link">Contact Us</a>
                             </div>
                         </div>
                     </div>
                 </div>
 
+                {/* Right side - User Account and Cart */}
                 <div className="nav-account">
-                    <div className="nav-item">
-                        <a href="/cart" className="nav-link">Cart</a>
-                    </div>
+                    <a href="/login" className="nav-link">
+                        <svg className="user-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
+                        </svg>
+                    </a>
+                    <a href="/cart" className="nav-link">
+                        <svg className="cart-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M7 18c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12L8.1 13h7.45c.75 0 1.41-.41 1.75-1.03L21.7 4H5.21l-.94-2H1zm16 16c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
+                        </svg>
+                    </a>
                 </div>
             </nav>
         </header>
